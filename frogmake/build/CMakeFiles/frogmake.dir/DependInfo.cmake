d0b08685e5d34a430f9e655e9d7334165244c5b5
nzeu <117606143+nzeu@users.noreply.github.com>
2024-01-27 16:39:42 -0500

    Add files via upload

---
+++ frogmake/build/CMakeFiles/frogmake.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nzeu/programing/frogmake/src/frogmake.c" "CMakeFiles/frogmake.dir/src/frogmake.c.o" "gcc" "CMakeFiles/frogmake.dir/src/frogmake.c.o.d"
+  "/home/nzeu/programing/frogmake/src/parsefile.c" "CMakeFiles/frogmake.dir/src/parsefile.c.o" "gcc" "CMakeFiles/frogmake.dir/src/parsefile.c.o.d"
+  "/home/nzeu/programing/frogmake/src/toml.c" "CMakeFiles/frogmake.dir/src/toml.c.o" "gcc" "CMakeFiles/frogmake.dir/src/toml.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ frogmake/build/CMakeFiles/frogmake.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/frogmake.dir/src/frogmake.c.o"
+  "CMakeFiles/frogmake.dir/src/frogmake.c.o.d"
+  "CMakeFiles/frogmake.dir/src/parsefile.c.o"
+  "CMakeFiles/frogmake.dir/src/parsefile.c.o.d"
+  "CMakeFiles/frogmake.dir/src/toml.c.o"
+  "CMakeFiles/frogmake.dir/src/toml.c.o.d"
+  "frogmake"
+  "frogmake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/frogmake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
